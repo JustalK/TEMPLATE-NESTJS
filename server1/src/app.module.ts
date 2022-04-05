ddb301c8deffaebd4e346b128150f8897daa69c3
justalk <justal.kevin@gmail.com>
2022-04-05 07:24:22 +0200

    [FIX] Fixing the database connection

--- server1/src/app.module.ts
+++ server1/src/app.module.ts
@@ -8,13 +8,10 @@ import { Experience0Module } from './experiences/experience0/experience0.module'
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 
-
-// mongodb://${process.env.MONGO_INITDB_ROOT_USERNAME}:${process.env.MONGO_INITDB_ROOT_PASSWORD}@localhost:${process.env.MONGO_PORT}/${process.env.MONGO_INITDB_DATABASE}
-
 @Module({
   imports: [
     Experience0Module,
-    MongooseModule.forRoot('mongodb://root:smood2mongo@localhost:27017/admin'),
+    MongooseModule.forRoot(`mongodb://${process.env.MONGO_INITDB_ROOT_USERNAME}:${process.env.MONGO_INITDB_ROOT_PASSWORD}@mongodb:${process.env.MONGO_PORT}/${process.env.MONGO_INITDB_DATABASE}?authSource=admin`),
     ConfigModule.forRoot({
       envFilePath: ['.env'],
       isGlobal: true,
