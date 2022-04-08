b9170348d10a61ecf368b1dc5e551a39a5a78a92
justalk <justal.kevin@gmail.com>
2022-04-08 19:44:14 +0200

    [FEATURE] Creating a mutate for create

--- server1/src/users/users.resolver.ts
+++ server1/src/users/users.resolver.ts
@@ -2,6 +2,7 @@ import { Args, Mutation, Query, Resolver, Subscription } from '@nestjs/graphql';
 import { User } from './models/user.model';
 import { NotFoundException } from '@nestjs/common';
 import { UsersService } from './users.service';
+import { CreateUserInput } from './dto/createUser.input';
 
 @Resolver((of) => User)
 export class UsersResolver {
@@ -14,5 +15,10 @@ export class UsersResolver {
       throw new NotFoundException(id);
     }
     return user;
+  }
+
+  @Mutation((returns) => User)
+  async upvotePost(@Args('CreateUserInput') createUserInput: CreateUserInput) {
+    return this.usersService.create(createUserInput);
   }
 }
--- server1/src/users/users.service.ts
+++ server1/src/users/users.service.ts
@@ -1,26 +1,16 @@
 import { Injectable } from '@nestjs/common';
 import { User } from './models/user.model';
 
+import createUserInput from './dto/createUsers.dto';
+
 @Injectable()
 export class UsersService {
-  private readonly users = [
-    {
-      id: '1',
-      firstname: 'FN1',
-      lastname: 'LN1',
-      username: 'john',
-      password: 'changeme',
-    },
-    {
-      id: '2',
-      firstname: 'FN2',
-      lastname: 'LN2',
-      username: 'maria',
-      password: 'guess',
-    },
-  ];
+  async findAll(): Promise<Cat[]> {
+    return this.userModel.find().exec();
+  }
 
-  async findOne(username: string): Promise<User | undefined> {
-    return this.users.find((user) => user.username === username);
+  async create(createUserInput: createUserInput): Promise<User> {
+    const createdUser = new this.User(createUserInput);
+    return createdUser.save();
   }
 }
