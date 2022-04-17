3526653eb6900c7257594e1db5421dba80c1b6a8
justalk <justal.kevin@gmail.com>
2022-04-17 17:56:38 +0200

    [FEATURE] Adding the args instead of type

--- server1/src/auth/auth.resolver.ts
+++ server1/src/auth/auth.resolver.ts
@@ -2,7 +2,7 @@ import { Args, Mutation, Resolver, createUnionType } from '@nestjs/graphql';
 import { User } from '../users/models/user.model';
 import { Payload } from './models/payload.model';
 import { AuthService } from './auth.service';
-import { LoginInput } from './dto/login.input';
+import { LoginArgs } from './dto/login.args';
 
 import { ObjectType, IntersectionType } from '@nestjs/graphql';
 @ObjectType()
@@ -12,9 +12,15 @@ export class ResultUnion extends IntersectionType(User, Payload) {}
 export class AuthResolver {
   constructor(private readonly authService: AuthService) {}
 
-  @Mutation(() => ResultUnion)
-  async login(@Args('LoginInput') loginInput: LoginInput) {
-    const { _doc: user } = await this.authService.validateUser('kevin', 'test');
+  @Mutation(() => ResultUnion, {
+    name: 'login',
+    description: 'Allow an user to connect to the app',
+  })
+  async login(@Args() loginArgs: LoginArgs) {
+    const { _doc: user } = await this.authService.validateUser(
+      loginArgs.username,
+      loginArgs.password,
+    );
     const payload = await this.authService.createPayload(user);
     return {
       ...user,
