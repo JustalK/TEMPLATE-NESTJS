5e063346d82725e9bc652f0c5e704365f3276e7e
justalk <justal.kevin@gmail.com>
2022-05-05 21:19:30 +0200

    [FEATURE] Adding fonction to mimic the nestjs

--- server1/src/shared/configs/services/winston-option.service.ts
+++ server1/src/shared/configs/services/winston-option.service.ts
@@ -3,38 +3,88 @@ import {
   WinstonModuleOptions,
   WinstonModuleOptionsFactory,
 } from 'nest-winston/dist/winston.interfaces';
-import { ConfigService } from '@nestjs/config';
 import * as winston from 'winston';
 import path from 'path';
 import colors from 'colors';
 
+const LOCAL_TIMESTAMP = 'en-GB';
+
+const CODE = '[CODE]';
+const PREFIXE = 'LOG';
+const SPACE = ' ';
+const TAB = '\t';
+const DASH = ' - ';
+const EMPTY = '';
+
+const NUMERIC = 'numeric';
+const DIGIT_TWO = '2-digit';
+
 @Injectable()
 export class WinstonOptionService implements WinstonModuleOptionsFactory {
-  constructor(private readonly configService: ConfigService) {}
+  prepareColorizedFormat(information: {
+    level: string;
+    timestamp: string;
+    filename: string;
+    message: string;
+    prefixe?: string;
+    code?: string;
+    suffixe?: string;
+  }) {
+    const code = colors.blue(information.code || CODE);
+    const level = colors.green(information.level);
+    const time = colors.white(information.timestamp);
+    const prefixe = colors.green(information.prefixe || PREFIXE);
+    const filename = colors.yellow(information.filename);
+    const message = colors.green(information.message);
+    const suffixe = colors.yellow(information.suffixe || EMPTY);
+    return EMPTY.concat(
+      code,
+      SPACE,
+      level,
+      SPACE,
+      DASH,
+      time,
+      TAB,
+      SPACE,
+      prefixe,
+      SPACE,
+      filename,
+      SPACE,
+      message,
+      SPACE,
+      suffixe,
+    );
+  }
+
+  prepareDateFormat(timestamp: Date): string {
+    return new Date(timestamp)
+      .toLocaleDateString(LOCAL_TIMESTAMP, {
+        year: NUMERIC,
+        month: DIGIT_TWO,
+        day: DIGIT_TWO,
+        hour12: true,
+        hour: NUMERIC,
+        minute: NUMERIC,
+        second: NUMERIC,
+      })
+      .toUpperCase();
+  }
+
+  prepareFilenameFormat(context: { __filename: string }): string {
+    return `[${path.basename(context.__filename)}]`;
+  }
 
   createWinstonModuleOptions():
     | Promise<WinstonModuleOptions>
     | WinstonModuleOptions {
-    const env = this.configService.get<string>('application.environment');
-
     const nestLikeFormat = winston.format.printf(
       ({ context, level, timestamp, message }) => {
-        return `${colors.blue('[Code]')} ${level}  ${colors.green(
-          '-',
-        )} ${new Date(timestamp)
-          .toLocaleDateString('en-GB', {
-            // you can use undefined as first argument
-            year: 'numeric',
-            month: '2-digit',
-            day: '2-digit',
-            hour12: true,
-            hour: 'numeric',
-            minute: 'numeric',
-            second: 'numeric',
-          })
-          .toUpperCase()}\t ${colors.green('LOG')} ${colors.yellow(
-          `[${path.basename(context.__filename)}]`,
-        )} ${colors.green(message)}`;
+        return this.prepareColorizedFormat({
+          level,
+          timestamp: this.prepareDateFormat(timestamp),
+          filename: this.prepareFilenameFormat(context),
+          message,
+        });
       },
     );
 
@@ -42,10 +92,7 @@ export class WinstonOptionService implements WinstonModuleOptionsFactory {
       transports: [
         new winston.transports.Console({
           format: winston.format.combine(
-            winston.format.colorize(),
-            winston.format.timestamp({
-              format: 'DD/MM/YYYY hh:mm:ss A',
-            }),
+            winston.format.timestamp(),
             nestLikeFormat,
           ),
         }),
