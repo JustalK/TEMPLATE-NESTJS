d92acbe32ff6c140e41f217cd9f7fcbbff887094
justalk <justal.kevin@gmail.com>
2022-05-06 17:28:53 +0200

    [DOCUMENTATION] Adding the constant and documentation

--- server1/src/shared/configs/services/winston-option.service.ts
+++ server1/src/shared/configs/services/winston-option.service.ts
@@ -3,24 +3,28 @@ import {
   WinstonModuleOptions,
   WinstonModuleOptionsFactory,
 } from 'nest-winston/dist/winston.interfaces';
+import * as CONSTANTS from '@shared/constants/string';
 import * as winston from 'winston';
 import path from 'path';
 import colors from 'colors';
 
-const LOCAL_TIMESTAMP = 'en-GB';
-
-const CODE = '[CODE]';
-const PREFIXE = 'LOG';
-const SPACE = ' ';
-const TAB = '\t';
-const DASH = ' - ';
-const EMPTY = '';
-
-const NUMERIC = 'numeric';
-const DIGIT_TWO = '2-digit';
-
+/**
+ * Injectable service for managing the winston logger
+ */
 @Injectable()
 export class WinstonOptionService implements WinstonModuleOptionsFactory {
+  /**
+   * Transform the context to a simple filename format such as [filename]
+   * @param {Object} information The information object
+   * @param information.level The level of the error
+   * @param information.timestamp The timestamp when the error has been called
+   * @param information.filename The name where the log has been called
+   * @param information.message The error message
+   * @param information.prefixe The message before the message
+   * @param information.code The code message
+   * @param information.suffixe The message after the message
+   * @return The message wrapped in the right color
+   * */
   prepareColorizedFormat(information: {
     level: string;
     timestamp: string;
@@ -29,55 +33,66 @@ export class WinstonOptionService implements WinstonModuleOptionsFactory {
     prefixe?: string;
     code?: string;
     suffixe?: string;
-  }) {
-    const code = colors.blue(information.code || CODE);
+  }): string {
+    const code = colors.blue(information.code || CONSTANTS.CODE);
     const level = colors.green(information.level);
     const time = colors.white(information.timestamp);
-    const prefixe = colors.green(information.prefixe || PREFIXE);
+    const prefixe = colors.green(information.prefixe || CONSTANTS.PREFIXE);
     const filename = colors.yellow(information.filename);
     const message = colors.green(information.message);
-    const suffixe = colors.yellow(information.suffixe || EMPTY);
-    return EMPTY.concat(
+    const suffixe = colors.yellow(information.suffixe || CONSTANTS.EMPTY);
+    return CONSTANTS.EMPTY.concat(
       code,
-      SPACE,
+      CONSTANTS.SPACE,
       level,
-      SPACE,
-      DASH,
+      CONSTANTS.SPACE,
+      CONSTANTS.DASH,
       time,
-      TAB,
-      SPACE,
+      CONSTANTS.TAB,
+      CONSTANTS.SPACE,
       prefixe,
-      SPACE,
+      CONSTANTS.SPACE,
       filename,
-      SPACE,
+      CONSTANTS.SPACE,
       message,
-      SPACE,
+      CONSTANTS.SPACE,
       suffixe,
     );
   }
 
   /**
+   * Transform a date to the same format as NestJS logger
    * @param {Date} timestamp The time that we want to beautify like in nestJS
    * @return {string} The new way to show the time in a beautify way
    * */
   prepareDateFormat(timestamp: Date): string {
     return new Date(timestamp)
-      .toLocaleDateString(LOCAL_TIMESTAMP, {
-        year: NUMERIC,
-        month: DIGIT_TWO,
-        day: DIGIT_TWO,
+      .toLocaleDateString(CONSTANTS.LOCAL_TIMESTAMP, {
+        year: CONSTANTS.NUMERIC,
+        month: CONSTANTS.DIGIT_TWO,
+        day: CONSTANTS.DIGIT_TWO,
         hour12: true,
-        hour: NUMERIC,
-        minute: NUMERIC,
-        second: NUMERIC,
+        hour: CONSTANTS.NUMERIC,
+        minute: CONSTANTS.NUMERIC,
+        second: CONSTANTS.NUMERIC,
       })
       .toUpperCase();
   }
 
+  /**
+   * Transform the context to a simple filename format such as [filename]
+   * @param {Object} context The context containing the metadata
+   * @param {string} context.__filename The path to the file
+   * @return {string} The filename wrapped between brackets
+   * */
   prepareFilenameFormat(context: { __filename: string }): string {
     return `[${path.basename(context.__filename)}]`;
   }
 
+  /**
+   * Create the transports for the whole application
+   * @return {Object} The transport for winston
+   * */
   createWinstonModuleOptions():
     | Promise<WinstonModuleOptions>
     | WinstonModuleOptions {
---
+++ server1/src/shared/constants/string.ts
@@ -0,0 +1,55 @@
+/**
+ * The local time of the app
+ * @constant
+ * @default
+ * */
+export const LOCAL_TIMESTAMP = 'en-GB';
+/**
+ * The code to show at the beginning of the log
+ * @constant
+ * @default
+ * */
+export const CODE = '[CODE]';
+/**
+ * The message to show before the message
+ * @constant
+ * @default
+ * */
+export const PREFIXE = 'LOG';
+/**
+ * A space
+ * @constant
+ * @default
+ * */
+export const SPACE = ' ';
+/**
+ * A simple tab in unix
+ * @constant
+ * @default
+ * */
+export const TAB = '\t';
+/**
+ * A simple dash for formatting
+ * @constant
+ * @default
+ * */
+export const DASH = ' - ';
+/**
+ * An empty string
+ * @constant
+ * @default
+ * */
+export const EMPTY = '';
+
+/**
+ * The Date constant for formatting a variable numerically
+ * @constant
+ * @default
+ * */
+export const NUMERIC = 'numeric';
+/**
+ * The Date constant for formatting a variable in 2 digits
+ * @constant
+ * @default
+ * */
+export const DIGIT_TWO = '2-digit';
